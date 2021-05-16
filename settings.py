e00488de36f1361a4c3fb93d7c125195c48f117e
Anisha Shruti A.T <anishashruti.at@gmail.com>
2021-05-16 18:24:22 +0530

    changes in settings.py

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ DEBUG_VALUE= os.environ.get('DEBUG_VALUE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['anishanetwork.herokuapp.com']
+ALLOWED_HOSTS = ['www.anishanetwork.herokuapp.com']
 
 
 # Application definition
